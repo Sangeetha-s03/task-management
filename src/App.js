aeb681cadfa97f760ea3ad5c1b2c93a9d1cb88f7
Vaseem06 <vaseemmohamed67@gmail.com>
2025-02-27 17:13:47 +0530

    day3 designs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+// src/pages/Header.jsx
+import React from 'react';
+import { useTable } from 'react-table'; // Correct import for react-table
+import { saveAs } from 'file-saver';   // Correct import for file-saver
+import './index.css';                 // Correct import for styles.css
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        This is a react app
-      </header>
-    </div>
+    <header>
+      <h1>Task Management System</h1>
+    </header>
   );
-}
+};
 
-export default App;
+export default Header;
